c47c30e57481eafcee9be8398cf8e80f994acd2c
Saqo <saqoagadzhanyan@gmail.com>
2015-02-15 23:56:33 +0400

    Create project

---
+++ OSStudio/OSStudio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OSStudio.Startup))]
+namespace OSStudio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
